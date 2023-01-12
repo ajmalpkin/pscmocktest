a5d0841e28c2a2d701aa126bb04abb2f6b5b1d1d
Ajmal Pk <ajmalpk100@gmail.com>
2023-01-12 18:04:08 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,107 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "മലബാറിൽ കീഴരിയൂർ ബോംബ് കേസ് നടന്ന വർഷം?",
+    answer: "1942",
+    options: [
+      "1944",
+      "1946",
+      "1940",
+      "1942"
+    ]
+  },
+    {
+    numb: 2,
+    question: "കേരളത്തിലെ ആദ്യ ശിശു സൗഹാർദ്ദ പഞ്ചായത്ത്?",
+    answer: "വെങ്ങാനൂർ",
+    options: [
+      "നെടുമ്പാശ്ശേരി ",
+      "വെങ്ങാനൂർ",
+      "കഞ്ഞിക്കുഴി",
+      "പള്ളിച്ചൽ"
+    ]
+  },
+    {
+    numb: 3,
+    question: "ചാന്നാർ ലഹളയ്ക്ക് പ്രചോദനമേകിയ സാമൂഹിക പരിഷ്കർത്താവ്?",
+    answer: "വൈകുണ്ഠസ്വാമികൾ ",
+    options: [
+      "ശ്രീനാരായണഗുരു",
+      "ചട്ടമ്പിസ്വാമികൾ",
+      "അയ്യങ്കാളി",
+      "വൈകുണ്ഠസ്വാമികൾ "
+    ]
+  },
+    {
+    numb: 4,
+    question: "സർദാർ സരോവർ ജലസേചന പദ്ധതി സ്ഥിതി ചെയ്യുന്ന സംസ്ഥാനം?",
+    answer: "ഗുജറാത്ത്",
+    options: [
+      "ഗോവ ",
+      "ഗുജറാത്ത്",
+      "മധ്യപ്രദേശ്",
+      "മഹാരാഷ്ട്ര"
+    ]
+  },
+    {
+    numb: 5,
+    question: " 1857ലെ മഹത്തായ വിപ്ലവത്തിന് നേതൃത്വം നൽകിയ മുഗൾ ചക്രവർത്തി ആര്?",
+    answer: "ബഹാദൂർ ഷാ II",
+    options: [
+      "ബഹദൂർഷാ I",
+      "സിറാജ് ഉദ് ദൗള ",
+      "ബഹാദൂർ ഷാ II",
+      "അഹമ്മദ് ഷാ"
+    ]
+  },
+  {
+    numb: 6,
+    question: " കൊച്ചിയെ ധനുഷ്കോടിയുമായി ബന്ധിപ്പിക്കുന്ന ദേശീയപാത ഏത്?",
+    answer: "NH 49",
+    options: [
+      "NH 47",
+      "NH 49",
+      "NH 17",
+      "NH 44"
+    ]
+  },
+  {
+    numb: 7,
+    question: "നീതി ആയോഗിന്റെ ആദ്യ വൈസ് ചെയർമാൻ ആര്?",
+    answer: "അരവിന്ദ് പനഗരിയ",
+    options: [
+      "സിന്ധു ശ്രീ ഖുള്ളർ",
+      "അമർത്യാസെൻ",
+      "രാജീവ് കുമാർ",
+      "അരവിന്ദ് പനഗരിയ"
+    ]
+  },
+  {
+    numb: 8,
+    question: "ഇന്ത്യയിലെ ഏറ്റവും പഴക്കം ചെന്ന വ്യവസായം എന്നറിയപ്പെടുന്നത്?",
+    answer: "പരുത്തി വ്യവസായം",
+    options: [
+      "പേപ്പർ വ്യവസായം",
+      "പഞ്ചസാര വ്യവസായം",
+      "കമ്പിളി വ്യവസായം",
+      "പരുത്തി വ്യവസായം"
+    ]
+  },
+    
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
